3635a7c7055074c7353e7025410cedbc5c84a050
rudrasiva86 <rudrasiva86@gmail.com>
2021-07-07 23:03:10 +0530

    @configuration - filters

--- src/main/java/com/rudrasiva86/config/AppConfig.java
+++ src/main/java/com/rudrasiva86/config/AppConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.rudrasiva86")
+//@ComponentScan(basePackages = "org.example",
+//includeFilters = @Filter(type = FilterType.REGEX, pattern = ".*Stub.*Repository"),
+//excludeFilters = @Filter(Repository.class))
 public class AppConfig {
 	
 }
