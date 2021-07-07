ddc37c00c9175c2e2f3835c77ab689f72018b003
rudrasiva86 <rudrasiva86@gmail.com>
2021-07-07 08:34:00 +0530

    Before spring

--- src/main/java/com/rudrasiva86/service/ProductService.java
+++ src/main/java/com/rudrasiva86/service/ProductService.java
@@ -4,15 +4,11 @@ import java.util.List;
 
 import com.rudrasiva86.model.Product;
 import com.rudrasiva86.repository.HibernateProductRepository;
-import com.rudrasiva86.repository.ProductRepository;
+import com.rudrasiva86.repository.IProductRepository;
 
 public class ProductService implements IProductService {
 	
-	private ProductRepository productRepository; 
-
-	public ProductService() {
-		this.productRepository = new HibernateProductRepository();
-	}
+	private IProductRepository productRepository = new HibernateProductRepository(); 
 
 	public List<Product> findAll() {
 		return productRepository.findAll();
